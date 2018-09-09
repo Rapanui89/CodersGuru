d55cd3daa4e71b0818e3543ff3781c89006615ae
Rapanui89 <akwilani@gmail.com>
2018-09-09 15:07:44 +0200

    Pierwsza wersja skryptów testowych dla strony codersguru

--- src/test/java/codersGuru/clickAllLinkOnWebsiteTest.java
+++ src/test/java/codersGuru/clickAllLinkOnWebsiteTest.java
@@ -1,12 +1,6 @@
 package codersGuru;
 
 import org.junit.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.assertEquals;
 
 //dziedziczenie po klasie BaseTest
 public class clickAllLinkOnWebsiteTest extends BaseTest {
@@ -16,8 +10,9 @@ public class clickAllLinkOnWebsiteTest extends BaseTest {
     public void clickAllElementsOnWebsite() {
         driver.get("https://men-men-s-01.codersguru.pl/");
         HomePage homePage = new HomePage(driver);
+        homePage.clicCodersLabLink();
         homePage.allLinkClick();
 
-    }
 
+    }
 }
